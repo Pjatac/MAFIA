f5bc64ff917ce6c66df06d36930e9ee9f0c6c65b
MS_VS <msvs@stddom.sela>
2019-12-24 15:55:27 +0200

    Added the client

---
+++ Client/src/app/user/sign-in/sign-in.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+
+import { UserService } from '../../shared/user.service';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent implements OnInit {
+
+  constructor(private userService: UserService,private router : Router) { }
+
+  model ={
+    username :'',
+    password:''
+  };
+  serverErrorMessages: string;
+  ngOnInit() {
+    if(this.userService.isLoggedIn())
+    this.router.navigateByUrl('/virtual-mashines');
+  }
+
+  onSubmit(form : NgForm){
+   
+    this.userService.login(form.value);
+    this.router.navigateByUrl('/virtual-mashines');
+  }
+}
