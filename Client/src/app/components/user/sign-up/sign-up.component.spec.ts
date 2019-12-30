880234480ccfdc657a0d8b312c4db06cc6c10c27
MS_VS <msvs@stddom.sela>
2019-12-30 11:53:41 +0200

    Added unit test to the client for login&register

--- Client/src/app/components/user/sign-up/sign-up.component.spec.ts
+++ Client/src/app/components/user/sign-up/sign-up.component.spec.ts
@@ -22,4 +22,48 @@ describe('SignUpComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('component initial state', () => {
+    expect(component.onSubmit).toBeFalsy();
+  });
+
+  it('submitted should be true when onSubmit()', () => {
+    expect(component.onSubmit).toBeTruthy();
+  });
+
+  it('userName field should display red outline', () => {
+    fixture.detectChanges();
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    const inputs = fixture.debugElement.nativeElement.querySelectorAll('input');
+    const userNamedInput = inputs[1];
+
+    expect(userNamedInput.classList).toContain('is-invalid');
+  });
+  
+  it('password field should display red outline', () => {
+    fixture.detectChanges();
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    const inputs = fixture.debugElement.nativeElement.querySelectorAll('input');
+    const passwordInput = inputs[1];
+
+    expect(passwordInput.classList).toContain('is-invalid');
+  });
+
+  it('confirm password field should display red outline', () => {
+    fixture.detectChanges();
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    const inputs = fixture.debugElement.nativeElement.querySelectorAll('input');
+    const cofirmPasswordInput = inputs[1];
+
+    expect(cofirmPasswordInput.classList).toContain('is-invalid');
+  });
 });
