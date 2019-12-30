5ca3a37bf63172c4174ead1c5d4b1846e290b235
ofri st <itsofri12@gmail.com>
2019-12-30 13:31:41 +0200

    Mock Server Linked

--- Server/mock/mock.js
+++ Server/mock/mock.js
@@ -1,13 +1,22 @@
 const ioClient = require('socket.io-client');
 
 var socketClient = ioClient("http://localhost:150");
-socketClient.on('getAllServers',(data) =>{
+console.log("Mock Server is up!");
+
+
+module.exports = {
+
+    Start: async () =>{
+        socketClient.on('getAllServers',(data) =>{
+        
+            console.log(data);
+            
+        });
+        socketClient.on('getNewServersData',(data) =>{
+            console.log(data);
+            
+        });
+    }
     
-    console.log(data);
-    
-});
-socketClient.on('getNewServersData',(data) =>{
-   console.log(data);
-   
-});
+}
 
