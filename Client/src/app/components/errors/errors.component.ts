79b38797ecc11a5feed7cdd3304b66f10dc3babd
Pjatak <pjataka@gmail.com>
2019-12-31 12:29:42 +0200

    ...

--- Client/src/app/components/errors/errors.component.ts
+++ Client/src/app/components/errors/errors.component.ts
@@ -13,29 +13,6 @@ export class ErrorsComponent implements OnInit {
   ngOnInit() {
     var chart = c3.generate({
       data: {
-<<<<<<< HEAD
-          columns: [
-              ['AuthMng/LogIn', 50],
-              ['ClientManager/Edit', 0],
-              ['ClientManager/Create', 110],
-              ['AuthMng/LogOut', 72]
-          ],
-          type: 'bar',
-          color : function (color, d) {
-            return "#0000FF";
-        },
-        labels: true
-      },
-      bar: {
-          width: {
-              ratio: 0.5 
-          },
-          space: 0.75
-          
-      },
-  });
-  }
-=======
         columns: [
             ['AuthMng/LogIn', 50],
             ['ClientManager/Edit', 0],
@@ -57,6 +34,5 @@ export class ErrorsComponent implements OnInit {
     },
 });
 }
->>>>>>> PjatakBranch
 
 }
--- Client/src/app/components/virtualmashines/multy-select/multy-select.component.ts
+++ Client/src/app/components/virtualmashines/multy-select/multy-select.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
-=======
 import { Component, Input, Output, EventEmitter } from '@angular/core';
->>>>>>> PjatakBranch
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -10,22 +6,11 @@ import { FormControl } from '@angular/forms';
   templateUrl: './multy-select.component.html',
   styleUrls: ['./multy-select.component.css']
 })
-<<<<<<< HEAD
-export class MultySelectComponent implements OnChanges {
-=======
 export class MultySelectComponent {
->>>>>>> PjatakBranch
   servers = new FormControl();
   @Input() srvList: string[];
   @Output() changeSelection = new EventEmitter();
   constructor() { }
-<<<<<<< HEAD
-  ngOnChanges() {
-    this.srvList = [];
-  }
-=======
-
->>>>>>> PjatakBranch
   newSelection(servers: string[], ms) {
     ms.close();
     this.changeSelection.emit(servers);
