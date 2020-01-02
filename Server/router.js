413d53861d4cd52476db85ec6f6f849ea1a57ef0
ofri st <itsofri12@gmail.com>
2020-01-02 10:14:17 +0200

    Added Logout

--- Server/router.js
+++ Server/router.js
@@ -8,45 +8,43 @@ module.exports = {
         io.on('connection', function (socket) {
 
             const RegisterRequest = async (data) => {
-                console.log("Register Request",data);
+                console.log("Register Request", data);
 
                 await UserSerivce.Register(data, socket);
             }
             const LoginRequest = async (data) => {
-                console.log("Login Request",data);
-                
+                console.log("Login Request", data);
+
                 let res = await UserSerivce.Login(data, socket);
                 if (res) {
-                    let c = { id: ids, session: socket,time:5,lastSentTime:Date.now() }
+                    let c = { id: ids, session: socket, time: 5, lastSentTime: Date.now() }
                     clients.push(c);
                     ids++;
                 }
             }
-            const ChartRequest = async (data) =>{
-                await MockService.GetChart(data,socket,clients.find(x=> x.session == socket));
+            const ChartRequest = async (data) => {
+                await MockService.GetChart(data, socket, clients.find(x => x.session == socket));
             }
-            const AddNewData = async (data) =>{
+            const AddNewData = async (data) => {
                 await MockService.AddNewMockData(data);
             }
-            const FbLoginReq = async (data) =>{
-                await UserSerivce.FBLogin(data,socket);
-            }
-            const GetAllServers = async () => {
-                await MockService.GetAllServers(socket);
+            const FbLoginReq = async (data) => {
+                await UserSerivce.FBLogin(data, socket);
             }
             console.log("a new user connected", clients.length);
 
-            socket.on('getAllServers', GetAllServers);
             socket.on('register-request', RegisterRequest);
             socket.on('login-request', LoginRequest);
             socket.on('fb-login-request', FbLoginReq);
-            socket.on('chart-request',ChartRequest)
-            socket.on('add-new-data',AddNewData)
-            socket.on('disconnect', function () {
-                clients.splice(clients.indexOf(x => x.session = socket), 1);
-                console.log('user disconnected', clients.length);
-            });
+            socket.on('chart-request', ChartRequest)
+            socket.on('add-new-data', AddNewData)
+            socket.on('logoutRequest',Disconnect);
+            socket.on('disconnect', Disconnect);
 
         });
     }
+}
+const Disconnect = function () {
+    clients.splice(clients.indexOf(x => x.session = socket), 1);
+    console.log('user disconnected', clients.length);
 }
