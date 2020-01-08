6e8aafbf438cf39fc9215b1ea736d5b909a0c9c0
Pjatak <pjataka@gmail.com>
2020-01-09 00:05:45 +0200

    change slider step to 1

--- Client/src/app/components/errors/range-slider/range-slider.component.ts
+++ Client/src/app/components/errors/range-slider/range-slider.component.ts
@@ -14,7 +14,7 @@ export class RangeSliderComponent  {
     ceil: 1440,
     ticksArray: [60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 660, 720, 780, 840, 900, 960, 1020, 1080, 1140, 1200, 1260, 1320, 1380],
     showSelectionBarEnd: false,
-    step: 5,
+    step: 1,
     getLegend: (value: number): string => {
       return value / 60 + "h";
     }
