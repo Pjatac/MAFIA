588a4fd7ee07d11f778c2312b69cf58caf14664b
Pjatak <pjataka@gmail.com>
2020-01-26 11:11:28 +0200

    Some cleare changes

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,9 +1,18 @@
-// built-in
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { SocialLoginModule, AuthServiceConfig, FacebookLoginProvider } from 'angularx-social-login';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { AngularMultiSelectModule} from 'angular2-multiselect-dropdown';
+import { ExportAsModule } from 'ngx-export-as';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Ng5SliderModule } from 'ng5-slider';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { TreeviewModule } from 'ngx-treeview';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 // components
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
@@ -11,20 +20,30 @@ import { SignInComponent } from './components/user/sign-in/sign-in.component';
 import { SignUpComponent } from './components/user/sign-up/sign-up.component';
 import { NavigationBarComponent } from './components/common_elements/navigation_bar/navigation_bar.component';
 import { NavigationBarLinkComponent } from './components/common_elements/navigation_bar/navigation_bar_link/navigation_bar_link.component';
+import { OurDialogComponent } from './components/shared/our-dialog/our-dialog.component';
+import { MemChartComponent } from './components/virtualmashines/mem-chart/mem-chart.component';
+import { CpuChartComponent } from './components/virtualmashines/cpu-chart/cpu-chart.component';
+import { MultySelectComponent } from './components/shared/multy-select/multy-select.component';
+import { SimpleSelectComponent } from './components/shared/simple-select/simple-select.component';
+import { TimesResponsesComponent } from './components/times-responses/times-responses.component';
+import { RespPieChartComponent } from './components/times-responses/resp-pie-chart/resp-pie-chart.component';
+import { TimePieChartComponent } from './components/times-responses/time-pie-chart/time-pie-chart.component';
+import { PieChartDialogComponent } from './components/times-responses/pie-chart-dialog/pie-chart-dialog.component';
+import { ErrorsComponent } from './components/errors/errors.component';
+import { OurDataPickerComponent } from './components/errors/our-data-picker/our-data-picker.component';
+import { RangeSliderComponent } from './components/errors/range-slider/range-slider.component';
+import { PrintLayoutComponent } from './components/print-layout/print-layout.component';
+import { TreeComponent } from './components/shared/tree/tree.component';
+import { VirtualmashinesComponent } from './components/virtualmashines/virtualmashines.component';
 // routes
 import { appRoutes } from './routes';
+//services
 import { UserService } from './services/user.service';
 import ErrService from '../app/services/err.service';
 import VmService from '../app/services/vm.service';
 import TrService from '../app/services/tr.service';
 import AuthService from './services/auth.service';
 import ScreenshotService from './services/screenshot.service';
-//other
-import { SocialLoginModule, AuthServiceConfig, FacebookLoginProvider } from 'angularx-social-login';
-import { VirtualmashinesComponent } from './components/virtualmashines/virtualmashines.component';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import { AngularMultiSelectModule} from 'angular2-multiselect-dropdown';
-import { ExportAsModule } from 'ngx-export-as';
 //Material
 import {
   MatIconModule,
@@ -43,29 +62,12 @@ import {
   MatDatepickerModule,
   MatChipsModule
 } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OurDialogComponent } from './components/shared/our-dialog/our-dialog.component';
-import { MemChartComponent } from './components/virtualmashines/mem-chart/mem-chart.component';
-import { CpuChartComponent } from './components/virtualmashines/cpu-chart/cpu-chart.component';
-import { MultySelectComponent } from './components/shared/multy-select/multy-select.component';
-import { SimpleSelectComponent } from './components/shared/simple-select/simple-select.component';
-import { TimesResponsesComponent } from './components/times-responses/times-responses.component';
-import { RespPieChartComponent } from './components/times-responses/resp-pie-chart/resp-pie-chart.component';
-import { TimePieChartComponent } from './components/times-responses/time-pie-chart/time-pie-chart.component';
-import { PieChartDialogComponent } from './components/times-responses/pie-chart-dialog/pie-chart-dialog.component';
-import { ErrorsComponent } from './components/errors/errors.component';
-import { OurDataPickerComponent } from './components/errors/our-data-picker/our-data-picker.component';
-import { RangeSliderComponent } from './components/errors/range-slider/range-slider.component';
-import { Ng5SliderModule } from 'ng5-slider';
-import { PrintLayoutComponent } from './components/print-layout/print-layout.component';
-import { NgxSpinnerModule } from "ngx-spinner";
-import { TreeviewModule } from 'ngx-treeview';
-import { TreeComponent } from './components/shared/tree/tree.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
-const ioConfig: SocketIoConfig = { url: 'localhost:100', options: {} };//http://91.205.172.45
 
+const ioConfig: SocketIoConfig = { url: 'http://localhost:100', options: {} };//http://91.205.172.45
+//try to add SSL connection
+//{ url: 'https://localhost:3003', options: {transports: ['websocket'],secure: true} }
 
 const authConfig = new AuthServiceConfig([
   {
--- Server/server.js
+++ Server/server.js
@@ -11,3 +11,36 @@ mockServer.Start();
 server.listen(PORT);
 //console.log();
 log.info("Server started on port ",PORT);
+
+// try to add Self_TRUSTED key and SSL connection
+// process.env.NODE_ENV = 'development';
+// const log = require('simple-node-logger').createSimpleLogger('server.log');
+// const ConnectToDb = require('./database');
+// const sessionRouter = require('./router');
+// const mockServer = require('./mock/mock');
+// const PORT = 3003;
+// ConnectToDb();
+
+// var fs = require('fs');
+// var https = require('https');
+// var path = require('path');
+
+// const server =
+//     https.createServer({
+//         key: fs.readFileSync(
+//            './ssl/localhost.key'
+//         ),
+//         cert: fs.readFileSync(
+//            './ssl/localhost.crt'
+//         ),
+//         requestCert: false,
+//         rejectUnauthorized: false
+//     });
+
+
+// const io = require('socket.io')(server);
+// server.listen(PORT);
+
+// sessionRouter.sessionRouter(io);
+// mockServer.Start();
+// log.info("Server started on port ", PORT);
